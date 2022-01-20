53fa1a3b150f01047f832e1fa48efaa03920dd0c
Petter <stureappleid@icloud.com>
2022-01-20 08:19:26 +0100

    lining things up

--- src/Box/user_Information.java
+++ src/Box/user_Information.java
@@ -3,7 +3,14 @@ package Box;
 public class user_Information {
     private String list;
     private String page;
-    user_Information[] _GET = new user_Information[]{new user_Information("customers", "2"), new user_Information("customers", "2"), new user_Information("customers", "2"), new user_Information("foo", "bar"), new user_Information("foo", "bar"), new user_Information("foo", "bar")};
+    user_Information[] _GET =
+            new user_Information[]{
+                    new user_Information("customers", "2"),
+                    new user_Information("customers", "2"),
+                    new user_Information("customers", "2"),
+                    new user_Information("foo", "bar"),
+                    new user_Information("foo", "bar"),
+                    new user_Information("foo", "bar")};
 
     public user_Information(String list, String page) {
         this.list = list;
