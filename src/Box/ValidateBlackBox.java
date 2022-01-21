402e7fe71580371424eeed8e7af3449bb7b9910b
CitronZoegas <81166713+CitronZoegas@users.noreply.github.com>
2022-01-21 17:38:28 +0100

    Fixed some spacing

--- src/Box/ValidateBlackBox.java
+++ src/Box/ValidateBlackBox.java
@@ -12,32 +12,41 @@ public class ValidateBlackBox {
 
     }
 
+    /**
+     *
+     * First we create '_GET' arrays,then the 'in' arrays and after that the 'out' arrays. At last we add all of the
+     * arrays to the main List called 'checks'
+     *
+     *
+     * @return ArrayList<List<Map<String, String>>>
+     */
     public ArrayList<List<Map<String, String>>> validation_seeking() {
 
         HashMap<String, String> GETValues0 = new HashMap<>();
-        System.out.println("GET0");
+
         GETValues0.put("list0", "customer");
         GETValues0.put("page0", "2");
         HashMap<String, String> GETValues1 = new HashMap<>();
-        System.out.println("GET1");
+
         GETValues1.put("list1", "customer");
         GETValues1.put("page1", "2");
         HashMap<String, String> GETValues2 = new HashMap<>();
-        System.out.println("GET2");
+
         GETValues2.put("list2", "customer");
         GETValues2.put("page2", "2");
         HashMap<String, String> GETValues3 = new HashMap<>();
-        System.out.println("GET3");
+
         GETValues3.put("list0", "customer");
         GETValues3.put("page0", "2");
         HashMap<String, String> GETValues4 = new HashMap<>();
-        System.out.println("GET4");
+
         GETValues4.put("list1", "customer");
         GETValues4.put("page1", "2");
         HashMap<String, String> GETValues5 = new HashMap<>();
-        System.out.println("GET5");
+
         GETValues5.put("list2", "customer");
         GETValues5.put("page2", "2");
+
         HashMap<String, String> in0 = new HashMap<>();
         in0.put("page0", "3");
         HashMap<String, String> in1 = new HashMap<>();
@@ -66,30 +75,37 @@ public class ValidateBlackBox {
         out4.put("out4", "bar=1");
         HashMap<String, String> out5 = new HashMap<>();
         out5.put("out5", "foo=1&bar=2");
+
         List<Map<String, String>> _GET0 = new ArrayList<>();
         _GET0.add(GETValues0);
         _GET0.add(in0);
         _GET0.add(out0);
+
         List<Map<String, String>> _GET1 = new ArrayList<>();
         _GET1.add(GETValues1);
         _GET1.add(in1);
         _GET1.add(out1);
+
         List<Map<String, String>> _GET2 = new ArrayList<>();
         _GET2.add(GETValues2);
         _GET2.add(in2);
         _GET2.add(out2);
+
         List<Map<String, String>> _GET3 = new ArrayList<>();
         _GET3.add(GETValues3);
         _GET3.add(in3);
         _GET3.add(out3);
+
         List<Map<String, String>> _GET4 = new ArrayList<>();
         _GET4.add(GETValues4);
         _GET4.add(in4);
         _GET4.add(out4);
+
         List<Map<String, String>> _GET5 = new ArrayList<>();
         _GET5.add(GETValues5);
         _GET5.add(in5);
         _GET5.add(out5);
+
         ArrayList<List<Map<String, String>>> checks = new ArrayList<>();
         checks.add(_GET0);
         checks.add(_GET1);
@@ -97,9 +113,8 @@ public class ValidateBlackBox {
         checks.add(_GET3);
         checks.add(_GET4);
         checks.add(_GET5);
-        PrintStream var10001 = System.out;
-        Objects.requireNonNull(var10001);
-        checks.forEach(var10001::println);
+
+        checks.forEach(System.out::println);
         return checks;
     }
 }
